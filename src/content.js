133ebd6b1bdecf8e6ac35097127638946680ae93
Ludwig Stumpp <42147848+LudwigStumpp@users.noreply.github.com>
2021-10-16 13:49:23 +0200

    Add eslint + airbnb and corrected code

--- src/content.js
+++ src/content.js
@@ -1,51 +1,50 @@
 const THRESHOLD = 0.5;
 
 function addScore(textElement, score) {
-    const span = document.createElement("span");
+  const span = document.createElement('span');
 
-    if (score >= THRESHOLD) {
-        span.style.color = 'red';
-    }
+  if (score >= THRESHOLD) {
+    span.style.color = 'red';
+  }
 
-    span.innerHTML = `Toxicity Score: ${score.toFixed(2)}<br>`;
-    textElement.prepend(span);
+  span.innerHTML = `Toxicity Score: ${score.toFixed(2)}<br>`;
+  textElement.prepend(span);
 }
 
 function happify(timeline) {
-    const tweets = timeline.children[0].querySelectorAll(":scope > div:not([happified])");
+  const tweets = timeline.children[0].querySelectorAll(':scope > div:not([happified])');
 
-    // TODO add to queue and process queue as different calls
-    if (tweets.length <= 3) {
-        return;
+  // TODO add to queue and process queue as different calls
+  if (tweets.length <= 3) {
+    return;
+  }
+
+  // TODO only textElements and textContents by map on line 34
+  const textContents = [];
+  const textElements = [];
+
+  for (let i = 0; i < tweets.length; i += 1) {
+    const tweet = tweets[i];
+    const textElement = tweet.querySelector('[lang=en]');
+    if (textElement != null) {
+      textElements.push(textElement);
+      textContents.push(textElement.textContent);
     }
+    tweet.setAttribute('happified', '');
+  }
 
-    // TODO only textElements and textContents by map on line 34
-    let textContents = [];
-    let textElements = [];
-
-    for (let i = 0; i < tweets.length; i++) {
-        const tweet = tweets[i];
-        const textElement = tweet.querySelector('[lang=en]')
-        if (textElement != null) {
-            textElements.push(textElement);
-            textContents.push(textElement.textContent)
-        }
-        tweet.setAttribute('happified', '');
+  chrome.runtime.sendMessage({ input: textContents }, (response) => {
+    for (let i = 0; i < textElements.length; i += 1) {
+      addScore(textElements[i], response.toxicityProbs[i]);
     }
-
-    chrome.runtime.sendMessage({ input: textContents }, function (response) {
-        for (let i = 0; i < textElements.length; i++) {
-            addScore(textElements[i], response.toxicityProbs[i]);
-        };
-    });
+  });
 }
 
-setInterval(function () {
-    const timeline = document.querySelector('[aria-label*="Timeline"]');
-    if (timeline) {
-        happify(timeline);
-    }
-
+setInterval(() => {
+  const timeline = document.querySelector('[aria-label*="Timeline"]');
+  if (timeline) {
+    happify(timeline);
+  }
 }, 100);
 
 // setInterval(function () {
