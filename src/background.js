a5678dce506b9db4833d9595d65244775a9c63c2
Ludwig Stumpp <42147848+LudwigStumpp@users.noreply.github.com>
2022-06-03 01:06:07 +0200

    Preprocess text first

--- src/background.js
+++ src/background.js
@@ -1,11 +1,15 @@
 import '@tensorflow/tfjs';
 import { load } from '@tensorflow-models/toxicity';
 
+function preprocess(text) {
+  return text.replace(/[^a-zA-Z. ]/g, '').toLowerCase();
+}
+
 load().then((model) => {
   chrome.runtime.onMessage.addListener(
     (request, sender, sendResponse) => {
       if (request.input) {
-        model.classify(request.input).then((predictions) => {
+        model.classify(request.input.map((el) => preprocess(el))).then((predictions) => {
           const toxicityResults = predictions[6].results;
 
           const toxicityProbs = [];
